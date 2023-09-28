23c2c8847845b73151b838bfe33dbc963a60fb6e
smileofthesun <vinadelmar7@protonmail.com>
2023-09-28 17:28:36 +0200

    first dwmblocks

---
+++ blocks.h
@@ -0,0 +1,29 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	/*{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},*/
+	/*{"",	"sb-tasks",	10,	26},*/
+	/* {"",	"sb-music",	0,	11}, */
+	{"",	"sb-news",		0,	6},
+	{"",	"sb-pacpackages",	0,	8},
+	/*{"",	"sb-torrent",	20,	7},*/
+	/* {"",	"sb-memory",	10,	14}, */
+	/* {"",	"sb-cpu",		10,	18}, */
+	/* {"",	"sb-moonphase",	18000,	17}, */
+	/*{"",	"sb-doppler",	0,	13},*/
+	/*{"",	"sb-forecast",	18000,	5},*/
+	/*{"",	"sb-mailbox",	180,	12},*/
+	{"",	"sb-internet",	5,	4},
+	/*{"",	"sb-nettraf",	1,	16},*/
+	{"",	"sb-battery",	5,	3},
+	{"",	"sbmy-volume",	0,	10},
+	{"",	"sb-clock",	60,	1},
+
+	/* {"",	"sb-iplocate", 0,	27}, */
+	/*{"",	"sb-help-icon",	0,	15}, */
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "\0";
+static unsigned int delimLen = 5;
